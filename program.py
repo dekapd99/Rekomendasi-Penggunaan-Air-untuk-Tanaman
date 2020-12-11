73f2a8c5bf3eec1a85e2604d7615a058fadcedef
DK <dekaprimatio@gmail.com>
2020-12-12 00:01:28 +0700

    dokumentasi

--- program.py
+++ program.py
@@ -1,5 +1,12 @@
+#Numpy digunakan untuk manipulasi array secara mudah dan cepat, bagian dari depedency untuk scikit-learn
+#Scikit-learn digunakan untuk membantu melakukan processing data ataupun melakukan training data untuk kebutuhan machine-learning.
+#pandas digunakan untuk untuk pengolahan data yang berkaitan dengan dataframe
+#xlrd digunakan untuk membaca data mentahan excel menjadi xml yang merupakan bagian dependency dari pandas
+
 def klasifikasi(suhu, cuaca):
+    #list merupakan struktur data pada python yang mampu menyimpan lebih dari satu data
     hasil = []
+    #append merupakan fungsi untuk menambahkan list baru dari belakang
     if cuaca.lower() == 'cerah':
         hasil.append(0)
     elif cuaca.lower() == 'panas':
@@ -22,7 +29,11 @@ def klasifikasi(suhu, cuaca):
 def preprocess(data_train):
     elemen = []
     features = []
+    #for single_data untuk menampung index
+    #in data_train berarti rangenya adalah banyak data yang dibuat dalam dataset
     for single_data in data_train:
+        #kita membungkus elemen dengan tuple yang berguna untuk menyimpan sekumpulan data yang berisikan berbagai macam nilai dan object
+        #sifat tuple adalah immutable artinya tidak bisa diubah/dihapus
         elemen.append(tuple(klasifikasi(single_data[1],single_data[0])))
 
         if single_data[2] >= 50 and single_data[2] <= 80:
@@ -33,9 +44,10 @@ def preprocess(data_train):
             features.append(2)
         elif single_data[2] < 50:
             features.append(3)
-
+    #return value elemen dan features untuk storing define masing-masing
     return elemen, features
 
+#menentukan validasi input yang akan digunakan pada saat menjalankan program
 def dataValid(suhu, cuaca):
     if ((cuaca.lower() == 'cerah' or cuaca.lower() == 'panas'
         or cuaca.lower() == 'berawan' or cuaca.lower() == 'mendung'
@@ -43,15 +55,21 @@ def dataValid(suhu, cuaca):
             return True
     return False
 
+#import pandas untuk bisa menggunakan dataframe
 import pandas as pd
 
+#read_excel merupakan fungsi dari library xlrd
+#values.tolist merupakan fungsi pandas untuk melakukan konversi datafram menjadi list
+#DataFrame merupakan struktur data 2 dimensi yang terdiri dari baris dan kolom
 data_train = pd.read_excel('dataset.xlsx').values.tolist()
 
 elemen, features = preprocess(data_train)
 
+#penggunaan naive bayes
 from sklearn.naive_bayes import GaussianNB
-
+# Mengaktifkan/memanggil/membuat fungsi klasifikasi Naive Bayes
 model = GaussianNB()
+# Memasukkan data training pada fungsi klasifikasi Naive Bayes
 model.fit(elemen, features)
 
 while(True):
@@ -61,6 +79,7 @@ while(True):
     print("Masukan Suhu (Celcius):")
     suhu = input()
     if(dataValid(suhu, cuaca)):
+        # Menentukan hasil prediksi, predict berdasarkan validasi dataset dengan membandingkan dengan data_train yang lalu
         pred = model.predict([klasifikasi(int(suhu), cuaca)]).tolist()
         print("Rekomendasi volume penyiraman air: ", end='')
         if pred[0] == 0:
